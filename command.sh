38dbc6831fb19b6bba26dd2c7c84bb0130cc7e5f
Cannizzaro_reaction <157903250+Cannizzaro-reaction@users.noreply.github.com>
2025-05-01 13:38:32 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,23 @@
-python markov_model.py --reads /home/bio/wcc_class/proj1/data/reads.fa --genomes /home/bio/wcc_class/proj1/data/genomes --test /home/bio/wcc_class/proj1/data/test.fa --seq_id_map /home/bio/wcc_class/proj1/data/seq_id.csv
- 
+# evaluate the result and get accuracy
+## optional parameters: --scores_output & --assignment_summary & --time
+python markov_model.py \
+    --reads data/test.fa \
+    --genomes data/genomes \
+    --output_csv classification_result.csv \
+    --scores_output score_detail.csv \
+    --k 7 \
+    --eval \
+    --seq_id_map data/seq_id.csv \
+    --assignment_summary assignment_summary.txt \
+    --time
+
+# only assign the reads (without evaluation)
+## optional parameters: --scores_output & --assignment_summary & --time
+python markov_model.py \
+    --reads data/reads.fa \
+    --genomes data/genomes \
+    --output_csv classification_result_noEval.csv \
+    --scores_output score_detail_noEval.csv \
+    --k 7 \
+    --assignment_summary assignment_summary_noEval.txt \
+    --time
